2e675d7a139f4f76a2a863997991f283886c6b0a
Taylor Luke (ETAS-DEV/XPC-UK)# <Luke.Taylor@etas.com>
2022-12-06 13:45:04 +0000

    Slightly optimized day6

--- day6/main.py
+++ day6/main.py
@@ -1,13 +1,13 @@
 from data import example, practice_1, practice_2, practice_3, practice_4, full_data
 
-def contains_duplicates(buffer):
-    return len(set(buffer)) == len(buffer)
+def contains_duplicates(buffer, buffer_size):
+    return len(set(buffer)) == buffer_size
 
 def find_unique_char_sequence(data, buffer_size):
     buffer = list(data[:buffer_size])
     marker=buffer_size
     for idx, char in enumerate(data[buffer_size:]):
-        if contains_duplicates(buffer):
+        if contains_duplicates(buffer, buffer_size):
             break
         buffer_idx = idx%buffer_size
         buffer[buffer_idx] = char
