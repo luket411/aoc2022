3cadd3bc953c2f7119e7c14ad4983cef05643edd
Taylor Luke (ETAS-DEV/XPC-UK)# <Luke.Taylor@etas.com>
2022-12-06 14:57:54 +0000

    Added CrateStacker10000 to day6

--- day6/main.py
+++ day6/main.py
@@ -3,6 +3,18 @@ from data import example, practice_1, practice_2, practice_3, practice_4, full_d
 def contains_duplicates(buffer, buffer_size):
     return len(set(buffer)) == buffer_size
 
+def contains_duplicates_slower(buffer, buffer_size):
+    match_found = False
+    for idx in range(buffer_size**2):
+        idx_1, idx_2 = idx//buffer_size, idx%buffer_size
+        if idx_1 >= idx_2:
+            continue
+        if buffer[idx_1] == buffer[idx_2]:
+            match_found=True
+            break
+    
+    return match_found
+
 def find_unique_char_sequence(data, buffer_size):
     buffer = list(data[:buffer_size])
     marker=buffer_size
@@ -22,16 +34,32 @@ def part_2(data=example):
 
 
 if __name__ == "__main__":
-    print(part_1())
-    print(part_1(practice_1))
-    print(part_1(practice_2))
-    print(part_1(practice_3))
-    print(part_1(practice_4))
-    print(part_1(full_data))
-    print("=============")
-    print(part_2())
-    print(part_2(practice_1))
-    print(part_2(practice_2))
-    print(part_2(practice_3))
-    print(part_2(practice_4))
-    print(part_2(full_data))
+    # print(part_1())
+    # print(part_1(practice_1))
+    # print(part_1(practice_2))
+    # print(part_1(practice_3))
+    # print(part_1(practice_4))
+    # print(part_1(full_data))
+    # print("=============")
+    # print(part_2())
+    # print(part_2(practice_1))
+    # print(part_2(practice_2))
+    # print(part_2(practice_3))
+    # print(part_2(practice_4))
+    # print(part_2(full_data))
+
+    import timeit
+
+    func_1_true = lambda: contains_duplicates([0,1,2,0],4)
+    func_1_false = lambda: contains_duplicates([0,1,2,3],4)
+
+    func_2_true = lambda: contains_duplicates_slower([0,0,2,2],4)
+    func_2_false = lambda: contains_duplicates_slower([0,1,2,3],4)
+
+    print(timeit.timeit(func_1_true))    
+    print(timeit.timeit(func_1_false))    
+    print(timeit.timeit(func_2_true))    
+    print(timeit.timeit(func_2_false))    
+
+    # print(timeit.timeit(lambda:part_1(full_data), number=10000))
+    # print(timeit.timeit(lambda:part_2(full_data), number=10000))
